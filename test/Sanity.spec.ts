9f8a79c4bdf08ef199d8d63d3646c8ee497c4646
Pat Sissons <patricksissons@gmail.com>
2016-07-30 18:49:55 -0700

    adding support for testing

---
+++ test/Sanity.spec.ts
@@ -0,0 +1,44 @@
+import * as chai from 'chai';
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+
+const should = chai.should();
+chai.use(sinonChai);
+
+describe('Sanity Tests', () => {
+  describe('for mocha', () => {
+    it('can create a test with describe and it', () => {
+      // intentionally empty
+    });
+  });
+
+  describe('for chai', () => {
+    it('can use should.equal to assert', () => {
+      should.equal(true, true);
+    });
+
+    it('can use should extensions to assert', () => {
+      true.should.eql(true);
+    });
+  });
+
+  describe('for sinon', () => {
+    it('can create stubs', () => {
+      const stub = sinon.stub();
+
+      stub();
+
+      should.equal(1, stub.callCount);
+    });
+  });
+
+  describe('for sinon-chai', () => {
+    it('can use should to assert sinon properties', () => {
+      const stub = sinon.stub();
+
+      stub();
+
+      stub.should.have.been.calledOnce;
+    });
+  });
+});
