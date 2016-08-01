8b7197fb7138e5bee369bddae7f7ebc11911f5f2
Pat Sissons <patricksissons@gmail.com>
2016-08-01 01:14:05 -0700

    fixing api surface

--- src/rxobj.ts
+++ src/rxobj.ts
@@ -1,12 +1,8 @@
-/// <reference path="../typings/index.d.ts" />
-
 // augmentations
 import './Extensions';
-
-// helper classes
-export { SubjectScheduler } from './SubjectScheduler';
 
 export { ReactiveApp } from './ReactiveApp';
 export { ReactiveState } from './ReactiveState';
 export { ReactiveEvent } from './ReactiveEvent';
 export { ReactiveProperty } from './ReactiveProperty';
+export { ReactiveObject } from './ReactiveObject';
