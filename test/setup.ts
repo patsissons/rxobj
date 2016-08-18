8d89ef3f0a01e2d61a5eed6191a186e10534be7f
Pat Sissons <patricksissons@gmail.com>
2016-08-17 22:26:21 -0700

    adding observable extension to simplify mocha tests on subscriptions

--- test/setup.ts
+++ test/setup.ts
@@ -2,7 +2,31 @@
 // import * as sinon from 'sinon';
 // import * as sinonChai from 'sinon-chai';
 
+import 'es6-shim';
 
+import { Observable } from 'rxjs/Observable';
+import { PartialObserver } from 'rxjs/Observer';
+import { Subscription } from 'rxjs/Subscription';
+
+declare module '~rxjs/Observable' {
+  interface Observable<T> {
+    mochaSubscribe(observerOrNext?: PartialObserver<T> | ((value: T) => void), done?: (error?: any) => any, error?: (error: any) => void, complete?: () => void): Subscription;
+  }
+}
+
+function mochaSubscribe<T>(observerOrNext?: (value: T) => void, done?: (error?: any) => any, error?: (error: any) => void, complete?: () => void): Subscription {
+  return (done == null) ?
+    (this as Observable<T>).subscribe(observerOrNext, error, complete) :
+    (this as Observable<T>).subscribe(x => {
+      try {
+        observerOrNext.apply(this, [ x ]);
+      } catch (e) {
+        done(e);
+      }
+    }, error, complete);
+}
+
+Observable.prototype.mochaSubscribe = mochaSubscribe;
 
 export default function setup(chai: Chai.ChaiStatic) {
   chai.use(require('sinon-chai'));
