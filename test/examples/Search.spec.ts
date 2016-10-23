8beeca775e160d2afb6959ac0fb6280180b68075
Pat Sissons <patricksissons@gmail.com>
2016-10-23 15:11:52 -0700

    updating the search example (and tests)

--- test/examples/Search.spec.ts
+++ test/examples/Search.spec.ts
@@ -1,40 +1,160 @@
-import { should } from '../setup';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { subscribeNotCalledError } from '../setup';
+import { Observable, Subject } from 'rxjs';
 import { SearchService, SearchViewModel } from './Search';
 
 describe('Search Example', () => {
-  function getResults(query: string) {
-    return Observable
-      .of([ 'A', 'B', 'C' ])
-      .map(x => (query == null || query === '') ? x : x.filter(y => y === query));
-  }
-
   const svc = <SearchService>{
-    getResults,
+    getResults: function getResults(query: string) {
+      return Observable
+        .of([ 'A', 'B', 'C' ])
+        .map(x => (query == null || query === '') ? x : x.filter(y => y === query));
+    },
   };
 
-  const vm = new SearchViewModel(svc);
+  let vm: SearchViewModel;
 
-  const errors = new BehaviorSubject<Error>(undefined);
-  const items = new BehaviorSubject<string[]>(undefined);
-  let searchCount = 0;
+  beforeEach(() => {
+    vm = new SearchViewModel(svc);
+  });
 
-  vm.thrownErrors.subscribe(errors);
-  vm
-    .whenAnyState(vm, x => x.searchResults, x => x.value)
-    .subscribe(items);
+  it('generates queryText changed notifications when queryText changes', (done) => {
+    const input = [ 'A', 'B', 'C' ];
+    const stop = new Subject();
+    let error = subscribeNotCalledError;
 
-  vm.search.results
-    .subscribe(() => ++searchCount);
+    vm
+      .whenAnyValue(x => x.queryText, x => x)
+      .takeUntil(stop)
+      .filter(x => (x || '').length > 0)
+      .toArray()
+      .finally(() => done(error))
+      .subscribe(x => {
+        error = null;
+        x.should.eql(input);
+      });
 
-  it('functions as expected', () => {
-    vm.queryText.value = 'B';
-    should.exist(items.value);
-    items.value.should.eql([ 'B' ]);
-    searchCount.should.eql(1);
+    input.forEach(x => {
+      vm.queryText.value = x;
+    });
 
-    vm.unsubscribe();
-    vm.closed.should.be.true;
-    vm.members.every(x => x.closed).should.be.true;
+    stop.next();
+  });
+
+  it('generates view model changed notifications for queryText when queryText changes', (done) => {
+    const input = [ 'A', 'B', 'C' ];
+    const stop = new Subject();
+    let error = subscribeNotCalledError;
+
+    vm
+      .whenAnyObservable(x => x.changed, x => x)
+      .takeUntil(stop)
+      .filter(x => x.value.name === 'queryText' && (x.value.value || '').length > 0)
+      .map(x => <string>x.value.value)
+      .toArray()
+      .finally(() => done(error))
+      .subscribe(x => {
+        error = null;
+        x.should.eql(input);
+      });
+
+    input.forEach(x => {
+      vm.queryText.value = x;
+    });
+
+    stop.next(true);
+  });
+
+  it('generates search results notifications when queryText changes', (done) => {
+    const input = [ 'A', 'B', 'C' ];
+    const stop = new Subject();
+    let error = subscribeNotCalledError;
+
+    vm
+      .whenAnyValue(x => x.search, x => x)
+      .takeUntil(stop)
+      .filter(x => (x || []).length > 0)
+      .toArray()
+      .finally(() => done(error))
+      .subscribe(x => {
+        error = null;
+        x.should.eql(input.map(y => [ y ]));
+      });
+
+    input.forEach(x => {
+      vm.queryText.value = x;
+    });
+
+    stop.next();
+  });
+
+  it('generates view model changed notifications for search results when queryText changes', (done) => {
+    const input = [ 'A', 'B', 'C' ];
+    const stop = new Subject();
+    let error = subscribeNotCalledError;
+
+    vm
+      .whenAnyObservable(x => x.changed, x => x)
+      .takeUntil(stop)
+      .filter(x => x.value.name === 'search' && (x.value.value || []).length > 0)
+      .map(x => <string>x.value.value)
+      .toArray()
+      .finally(() => done(error))
+      .subscribe(x => {
+        error = null;
+        x.should.eql(input.map(y => [ y ]));
+      });
+
+    input.forEach(x => {
+      vm.queryText.value = x;
+    });
+
+    stop.next(true);
+  });
+
+  it('generates searchResults notifications when queryText changes', (done) => {
+    const input = [ 'A', 'B', 'C' ];
+    const stop = new Subject();
+    let error = subscribeNotCalledError;
+
+    vm
+      .whenAnyValue(x => x.searchResults, x => x)
+      .takeUntil(stop)
+      .filter(x => (x || []).length > 0)
+      .toArray()
+      .finally(() => done(error))
+      .subscribe(x => {
+        error = null;
+        x.should.eql(input.map(y => [ y ]));
+      });
+
+    input.forEach(x => {
+      vm.queryText.value = x;
+    });
+
+    stop.next();
+  });
+
+  it('generates view model changed notifications for searchResults when queryText changes', (done) => {
+    const input = [ 'A', 'B', 'C' ];
+    const stop = new Subject();
+    let error = subscribeNotCalledError;
+
+    vm
+      .whenAnyObservable(x => x.changed, x => x)
+      .takeUntil(stop)
+      .filter(x => x.value.name === 'searchResults' && (x.value.value || []).length > 0)
+      .map(x => <string>x.value.value)
+      .toArray()
+      .finally(() => done(error))
+      .subscribe(x => {
+        error = null;
+        x.should.eql(input.map(y => [ y ]));
+      });
+
+    input.forEach(x => {
+      vm.queryText.value = x;
+    });
+
+    stop.next(true);
   });
 });
